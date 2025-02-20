a1a099494a577202cddae19c3c4d17d830964915
hetashi <101481527+hetasshi@users.noreply.github.com>
2025-02-20 09:49:47 +0300

    Update 1.cpp

--- homework/1.cpp
+++ homework/1.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
 
+class Seconds {
+private:
+    int seconds;
+public:
+    Seconds(int s = 0) : seconds(s) {}
+    void setSeconds(int s) { seconds = s; }
+    int getSeconds() const { return seconds; }
+    bool operator==(const Seconds& other) const {
+        return seconds == other.seconds;
+    }
+};
+
+void modifySeconds(Seconds& s, int newVal) {
+    s.setSeconds(newVal);
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    Seconds a(15), b(15);
+    modifySeconds(b, 15);
+
+    if (a == b) {
+        cout << "Объекты равны" << endl;
+    }
+    else {
+        cout << "Объекты различны" << endl;
+    }
+
+    return 0;
+}
