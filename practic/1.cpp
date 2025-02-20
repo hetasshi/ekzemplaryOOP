553471a5b0cac4002b24a4303d68081405da9c41
hetashi <101481527+hetasshi@users.noreply.github.com>
2025-02-20 09:43:47 +0300

    Update 1.cpp

--- practic/1.cpp
+++ practic/1.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Seconds {
+private:
+    int seconds;
+public:
+    Seconds(int s = 0) : seconds(s) {}
+    void setSeconds(int s) { seconds = s; }
+    int getSeconds() const { return seconds; }
+    string toString() const { return to_string(seconds); }
+};
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int size;
+    cout << "Введите размер массива: ";
+    cin >> size;
+
+    Seconds** arr = new Seconds * [size];
+    for (int i = 0; i < size; ++i) {
+        arr[i] = new Seconds(i * 10);
+    }
+
+    cout << "Значения в массиве:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i]->toString() << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < size; ++i) {
+        delete arr[i];
+    }
+    delete[] arr;
+
+    return 0;
+}
