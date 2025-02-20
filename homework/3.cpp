798dbba126b3af9981c168026718cca67d04508f
hetashi <101481527+hetasshi@users.noreply.github.com>
2025-02-20 10:03:21 +0300

    Update 3.cpp

--- homework/3.cpp
+++ homework/3.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+class Seconds {
+private:
+    int seconds;
+public:
+    Seconds(int s = 0) : seconds(s) {}
+};
+
+int main() {
+    // создадим объект без имени, типа утечка памяти
+    new Seconds(10);
+
+    // чем это черевато?
+    // объект создаётся просто в куче, но указатель на него не сохраняется,
+    // поэтому его и нельзя удалить) так и случается утечка памяти
+
+    return 0;
+}
