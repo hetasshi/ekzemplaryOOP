654a76d20c191c8245f78d51ccd8b0a86f050492
hetashi <101481527+hetasshi@users.noreply.github.com>
2025-02-20 09:54:49 +0300

    Update 2.cpp

--- homework/2.cpp
+++ homework/2.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Seconds {
+private:
+    int seconds;
+public:
+    Seconds(int s = 0) : seconds(s) {}
+    void setSeconds(int s) { seconds = s; }
+    string toString() const { return to_string(seconds); }
+};
+
+int main() {
+    Seconds* s1 = new Seconds(70);
+    Seconds* s2 = new Seconds(300);
+
+    cout << "s1: " << s1->toString() << endl;
+    cout << "s2: " << s2->toString() << endl;
+
+    delete s1;
+    delete s2;
+
+    return 0;
+}
